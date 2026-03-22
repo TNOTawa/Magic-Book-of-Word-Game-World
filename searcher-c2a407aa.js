3e37b4c99540f36e9055711375b602b7835b3e62
TNOTawa <TNOTawa@users.noreply.github.com>
2026-03-22 12:30:52 +0000

    deploy: 61d96f19cfe3adc22d5c8d4a36eec629af8ed82b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5558fdf4.js',
+                path_to_root + 'searchindex-bbe2c347.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
